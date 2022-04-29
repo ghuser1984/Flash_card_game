3cefe792e4adad68576a2c4061c5a457fc48e067
Vasya Boeva <vs99516@gmail.com>
2022-04-29 16:01:33 +0300

    also mess

--- Model.js
+++ Model.js
@@ -1,58 +1,53 @@
 const readlineSync = require('readline-sync');
 const fs = require('fs');
-// const { check } = require('prettier');
-const topics = fs.readdirSync('./topics');
 
-class Topic {
-  constructor(topicNumber) {
-    this.topicName = topics[topicNumber - 1];
-    this.questions = getCards(topicNumber);
-  }
-}
+function runCode() {
+  const topics = fs.readdirSync('./topics');
 
-function getCards(topicNumber) {
-  let questions1 = fs.readFileSync(`./topics/${topics[topicNumber]}`, 'utf-8');
-  questions1 = questions1.split('\n\n').map((a) => a.split('\n')).map((a) => a.filter((el) => el)).map((arr) => toObj(arr));
-  return questions1;
-}
-function toObj(arr) {
-  const [question, answer] = arr;
-  return {
-    question,
-    answer,
-  };
-}
-// //fs.readFile(`./topisc/${topics[topicNumber]}`
-let rightAnswers = 0;
-let wrongAnswers = 0;
-for (let j = 0; j <= topics.length - 1; j++) {
-  const topicNumber = readlineSync.keyInSelect(topics, 'Wich topic?');
-  const checkTopic = new Topic(topicNumber);
-  for (let i = 0; i <= checkTopic.questions.length - 1; i++) {
-    const questionAsked = readlineSync.question(`${checkTopic.questions[i].question}\n`);
-    if (questionAsked === checkTopic.questions[i].answer) {
-      console.log('You are right');
-      rightAnswers += 1;
-    } else {
-      console.log('wrong');
-      wrongAnswers += 1;
+  class Topic {
+    constructor(topicNumber) {
+      this.topicName = topics[topicNumber - 1];
+      this.questions = getCards(topicNumber);
     }
   }
-  console.log(`You are done. Right Answers: ${rightAnswers}. Wrong answers: ${wrongAnswers}`);
+
+  function getPrettyName(text) {
+    return text.replace('.txt', '').split('_').join(' ');
+  }
+
+  function getCards(topicNumber) {
+    let questions1 = fs.readFileSync(`./topics/${topics[topicNumber]}`, 'utf-8');
+    questions1 = questions1.split('\n\n').map((a) => a.split('\n')).map((a) => a.filter((el) => el)).map((arr) => toObj(arr));
+    return questions1;
+  }
+  function toObj(arr) {
+    const [question, answer] = arr;
+    return {
+      question,
+      answer,
+    };
+  }
+
+  let rightAnswers = 0;
+  let wrongAnswers = 0;
+  for (let j = 0; j <= topics.length - 1; j += 1) {
+    const topicNumber = readlineSync.keyInSelect(topics, 'Wich topic?');
+    if (topicNumber === -1) { console.log('Ok,bye quitter 🪰'); return; }
+    const checkTopic = new Topic(topicNumber);
+    for (let i = 0; i <= checkTopic.questions.length - 1; i += 1) {
+      const questionAsked = readlineSync.question(`${checkTopic.questions[i].question}\n`);
+      if (questionAsked === checkTopic.questions[i].answer) {
+        console.log('You are right');
+        rightAnswers += 1;
+      } else {
+        console.log('wrong!! ' + `Right answer is ${checkTopic.questions[i].answer}`);
+        wrongAnswers += 1;
+      }
+    }
+    console.log(`You are done. Right Answers: ${rightAnswers}. Wrong answers: ${wrongAnswers}`);
+    rightAnswers = 0;
+    wrongAnswers = 0;
+  }
 }
 
-// console.log(checkTopic);
-
-// function getCards(topicNumber) {
-//   const cards = fs.readFile(`./topisc/${topics[topicNumber]}`);
-//   return cards;
-// }
-
-// console.log(checkTopic);
-// class Model {
-//   constructor() {
-
-//   }
-// }
-// // comment
-// module.exports = Model;
+module.exports = runCode;
--- runner.js
+++ runner.js
@@ -1,9 +1,5 @@
-const Controller = require("./Controller");
-const Model = require("./Model");
-const View = require("./View");
+const Controller = require('./Controller');
+const runCode = require('./Model');
+const View = require('./View');
 
-const model = new Model
-const view = new View
-const controller = new Controller(model, view)
-
-controller.run()
+runCode();
